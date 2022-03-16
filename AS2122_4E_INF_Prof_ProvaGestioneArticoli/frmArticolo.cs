852bf44c7693cb760f589a2f22c02392e48ecac5
PC13 <PC13@INFO5-5384>
2022-03-16 10:10:23 +0100

    commit punto 5 ,6,7

--- AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
+++ AS2122_4E_INF_Prof_ProvaGestioneArticoli/frmArticolo.cs
@@ -16,6 +16,9 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
 
         // TODO: (5) aggiungere attributi privati dei dati inseriti nella frmArticoli
         // ...
+        private string Descrizione { get; private set; }
+        private string UnitaMisura { get; private set; }
+        private double Prezzo { get; private set; }
 
 
         public DialogResult Status { get { return status; } }
@@ -31,7 +34,16 @@ namespace AS2122_4E_INF_Prof_ProvaGestioneArticoli
         {
             // TODO: (7) passaggio all' attributo/property dei dati inseriti nella frmArticoli con controllo di valorizzazione del dato
             // ... descrizione, unitaMisura, prezzo
-
+            if (txtPrezzo.text != "" && txtDescrizione.text != "" & cmbUnitaMisura.text != "")
+            {
+                Descrizione = txtDescrizione.text;
+                UnitaMisura = cmbUnitaMisura;
+                Prezzo = txtPrezzo;
+            }
+            else
+            {
+                Message.Box("Inserisci tutti i dati");
+            }
             status = DialogResult.OK;
             Close();
         }
